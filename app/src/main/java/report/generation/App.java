ad8e1f05c343a58b8113a0d53acced11820d6ccc
Vy <z3n0n3005@gmail.com>
2023-11-22 13:55:43 +0700

    Added some modification

--- app/src/main/java/report/generation/App.java
+++ app/src/main/java/report/generation/App.java
@@ -3,44 +3,12 @@
  */
 package report.generation;
 
-import org.grobid.core.*;
-import org.grobid.core.data.*;
-import org.grobid.core.factory.*;
-import org.grobid.core.main.*;
-import org.grobid.core.utilities.*;
-import org.grobid.core.engines.Engine;
-import java.util.Arrays;
-
 public class App {
-    public void grodid() {
-        // String pdfPath = "paper\\automatic-text-summarization-a-comprehensive-survey.pdf";
-        String pdfPath = "paper/automatic-text-summarization-a-comprehensive-survey.pdf";
-        try {
-            // String pGrobidHome = "C:\\Users\\DELL\\Prototype\\grobid-0.7.3\\grobid-home";
-            String pGrobidHome = "/media/vy/vy/prototype/grobid-0.7.3/grobid-home";
-            // If the location is customised:
-            GrobidHomeFinder grobidHomeFinder = new GrobidHomeFinder(Arrays.asList(pGrobidHome));
-
-            // The grobid yaml config file needs to be instantiate using the correct
-            // grobidHomeFinder or it will use the default
-            // locations
-            GrobidProperties.getInstance(grobidHomeFinder);
-
-            System.out.println(">>>>>>>> GROBID_HOME=" + GrobidProperties.getGrobidHome());
-
-            Engine engine = GrobidFactory.getInstance().createEngine();
-
-            // Biblio object for the result
-            BiblioItem resHeader = new BiblioItem();
-            String tei = engine.processHeader(pdfPath, 1, resHeader);
-            System.out.println(tei);
-        } catch (Exception e) {
-            // If an exception is generated, print a stack trace
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        new App().grodid();
+        System.out.println("[App] Start timing");
+        long startTime = System.nanoTime();
+        new PdfSegmentation().pdfSegmenting();
+        long endTIme = System.nanoTime();
+        System.out.println("[App] Time elapsed = " + ((endTIme - startTime)/100000) + "ms");
     }
 }
