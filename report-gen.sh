32f17d1baa3e7ecff3522fe63f48681c791b8475
Vy <z3n0n3005@gmail.com>
2024-01-05 23:21:20 +0700

    Fixed the bug

--- report-gen.sh
+++ report-gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 current_directory=$(pwd)
+# current_directory="/home"
 
 while [[ $1 =~ ^- && ! $1 == "--" ]]
   do 
@@ -10,30 +11,39 @@ while [[ $1 =~ ^- && ! $1 == "--" ]]
       --folder )
         shift; folder=$1
         ;;
+      -a | --algo )
+        shift; algo=$1
     esac; shift;
 done
 
-# If both folder and file are not provided
+# check algo type first
+if [[ -z "$algo" ]]; then
+  echo "error: algorithm type is not allowed to be null"
+  exit 1
+elif [[ "$algo" != "textrank" 
+    && "$algo" != "lsa" ]]; then 
+  echo "error: inappropriate algo value: "
+  exit 1
+fi
+ 
+# if both folder and file are not provided
 if [[ -z "$file"  && -z "$folder" ]]; then
-  echo "ERROR: No option has been provided"
+  echo "error: no file option has been provided"
+  exit 1
 
-# If both folder and file are provided
+# if both folder and file are provided
 elif [[ ! -z "$file" && ! -z "$folder" ]]; then
-  echo "ERROR: Only a file or a folder can be provided at the same time"
+  echo "error: only a file or a folder can be provided at the same time"
+  exit 1
 
-# If file is provided
+# if file is provided
 elif [[ ! -z "$file" && -z "$folder" ]]; then 
-  $relative_file="$current_directory"/"$file"
-  "$current_directory"/gradlew run -Pfile=$relative_file
-
+  relative_file="$current_directory/$file"
+  # "$current_directory"/gradlew run -Pfile="$current_directory/$file" -Palgo="$algo"
+  "$current_directory"/gradlew run -Pfile="$relative_file" -Palgo="$algo"
 # If folder is provided
 elif [[ -z "$file" && ! -z "$folder" ]]; then
-  $relative_folder="$current_directory/$folder"
-  echo "$relative_folder"
-  ls $current_directory
-  ls $relative_folder
-  "$current_directory"/gradlew run -Pfolder=$relative_folder
+  relative_folder="$current_directory/$folder"
+  # "$current_directory"/gradlew run -Pfolder="$current_directory/$folder" -Palgo="$algo"
+  "$current_directory"/gradlew run -Pfolder="$relative_folder" -Palgo="$algo"
 fi
-# import file name through command line later
-# "$current_directory"/gradlew run -Pfile=$file
-``
