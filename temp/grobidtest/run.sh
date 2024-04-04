b8afdfff0e00be1ecf93466eeb444564003b2c2d
Vy <z3n0n3005@gmail.com>
2024-04-04 07:19:02 +0700

    Sync

--- temp/grobidtest/run.sh
+++ temp/grobidtest/run.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 # mv /src/lib/grobid_client_python/grobid_client /src/grobid_client
 echo "Start running grobid client script"
-python grobid.py
-ls /src/resources
-ls /src/resources/test_out
+python src/app.py
+ls /code/resources
+ls /code/resources/test_out
