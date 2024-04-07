007786da2a012312af3099c079c9404d509f510f
Tri Vy Luong <trivyluong@gmail.com>
2024-04-07 13:12:38 +0700

    Update flask app + set up an quick way to test the pdf being sent over http

--- temp/grobidtest/run.sh
+++ temp/grobidtest/run.sh
@@ -1,6 +1,5 @@
 #!/bin/ash
-# mv /src/lib/grobid_client_python/grobid_client /src/grobid_client
-echo "Start running grobid client script"
+# echo "Start running grobid client script"
 flask run --debug
 # python src/app.py
 ls /code/resources
