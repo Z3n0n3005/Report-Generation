583fbd0625622be900bee61e01282e97e2a168e7
Tri Vy Luong <trivyluong@gmail.com>
2024-04-08 21:58:25 +0700

    Can push pdf to docker server + summarize it in batch successfully

--- temp/grobidtest/run.sh
+++ temp/grobidtest/run.sh
@@ -1,6 +1,11 @@
 #!/bin/ash
 # echo "Start running grobid client script"
+# pwd
+# ls /src
+# ls /src/resources
+# ls /src/resources/pdf_in
 flask run --debug
 # python src/app.py
+
 ls /code/resources
 ls /code/resources/test_out
--- temp/grobidtest/src/app.py
+++ temp/grobidtest/src/app.py
@@ -1,52 +1,78 @@
-from grobid import parse_pdf
-from flask import Flask, redirect, flash, render_template, url_for, request, Response
 import os
+from grobid import parse_pdf
+from flask import Flask, redirect, flash, render_template, url_for, request
+from requests import Response
 from werkzeug.utils import secure_filename
 
-# UPLOAD_FOLDER = 'src/resources/pdf_in'
-UPLOAD_FOLDER = 'resources\\pdf_in'
+UPLOAD_FOLDER = '/code/resources/pdf_in'
+SEGMENT_FOLDER = '/code/resources/pdf_segment'
 ALLOWED_EXTENSIONS = set(['pdf'])
 
+# UPLOAD_FOLDER = 'resources\\pdf_in'
+# SEGMENT_FOLDER = 'resources\\pdf_segment'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'secret_key'
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-           
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    
 @app.route("/")
 def hello_world():
-    flash("hi")
     return '<p>Hello World!</p>'
 
-@app.route("/summarize", methods=['GET', 'POST'])
+@app.route("/upload", methods=['POST'])
+def upload():
+    if (request.method != 'POST'):
+        return "No GET method"
+
+    # If header 'files' is not in the payload 
+    if 'files' not in request.files:
+        print('No pdf file')
+        return Response(
+            response="No 'files' header in payload.",
+            status=404
+        )
+        
+    pdf_file = request.files['files']
+
+    # If the file has no name
+    if pdf_file.filename == '':
+        print('No pdf file selected')
+        return Response(
+            response="File has no name.",
+            status=404
+        )
+        
+    # If the file does not exist 
+    if not pdf_file: 
+        return Response(
+            response="File does not exist.",
+            status=404
+        )
+        
+    # If the extension does not end with .pdf
+    if not allowed_file(pdf_file.filename):
+        return Response(
+            response="File does not end with .pdf",
+            status=404
+        )    
+
+    filename = secure_filename(pdf_file.filename)
+    pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+    print("END POST")
+    return 'Successfully send files'
+
+@app.route("/summarize", methods=['GET'])
 def summarize():
-    if(request.method == 'POST'):
-        # check if the post request has the file part
-        print("request files:", request.files)
-        if 'files' not in request.files:
-            print('No pdf file')
-            return Response(
-                response="<p>No pdf file</p>",
-                status=404
-            )
-        for file in request.files:
-            print(file)
-        pdf_file = request.files['files']
-
-        if pdf_file.filename == '':
-            print('No pdf file selected')
-            flash('No pdf file selected')
-            return redirect(request.url)
-        if pdf_file and allowed_file(pdf_file.filename):
-            filename = secure_filename(pdf_file.filename)
-            pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print("END POST")
-        return 'Successfully send files'
-    else:
-        print("END GET")
-        return 'Get sth i dunno'
-
+    parse_pdf()
+    return "Hi"
+    # return Response(
+    #     response="Complete",
+    #     status=200
+    # )
 
 def main():
     parse_pdf()
@@ -55,5 +81,5 @@ def main():
 if __name__ == "__main__":
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.secret_key = "secret_key"
-    app.run(debug=True)
+    app.run(debug=False)
     # main()
