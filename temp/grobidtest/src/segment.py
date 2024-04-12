889581d39e207250f28338238e76fb0f47ba08e2
Tri Vy Luong <trivyluong@gmail.com>
2024-04-12 08:54:54 +0700

    Checkpoint xml-parser, update segment and paper

--- temp/grobidtest/src/segment.py
+++ temp/grobidtest/src/segment.py
@@ -3,6 +3,24 @@ class Segment:
         self.header = header
         self.content = content
         
+    def __init__(self):
+        self.header = ""
+        self.content = ""
+
+    def __str__(self) -> str:
+        result = "-----------\n"
+        result += "HEADER: " 
+        result += self.header + "\n"
+        result += "CONTENT: \n"
+        result += self.content + "\n"
+        return result
+
+    def set_header(self, header:str):
+        self.header = header
+    
+    def set_content(self, content:str):
+        self.content = content
+
     def get_header(self):
         return self.header
 
--- temp/grobidtest/src/xml_parser.py
+++ temp/grobidtest/src/xml_parser.py
@@ -1,29 +1,77 @@
 import config
 import os
 from paper import Paper
+from segment import Segment
 import xml.etree.ElementTree as ET
 
 SEGMENT_FOLDER = config.get_segment_path()
 
 def parse_xml_folder():
     files = []
-    print(os.getcwd() + SEGMENT_FOLDER)
+    papers:list[Paper] =[]
     # Get all file path list
     for (dirpath, _, filenames) in os.walk(SEGMENT_FOLDER):
         for filename in filenames:
             if filename.endswith('.tei.xml'):
                 files.append(os.sep.join([dirpath, filename]))
-    print(files)
     # May put threadpoolexecutor here if its slow
     for file in files:
-        parse_xml_file(file)
+        # Create new paper
+        paper = get_xml_parsing_result(file)
+        # Append paper to list
+        papers.append(paper)
 
-def parse_xml_file(file):
-    tree = ET.parse(file)    
+def get_xml_parsing_result(file) -> Paper:
+    paper = Paper()
+    paper.set_abstract_seg(get_abstract(file))
+    
+    paper.set_segment_list(get_segment_list(file))
+    
+    
+    
+    return paper
+
+def get_abstract(file) -> str:
+    result = ""
+    namespace = get_namespace()
+    tree = ET.parse(file)
     root = tree.getroot()
-    print(root)
+    for abstract in root.findall(".//tei:abstract", namespace):
+        for div in abstract.findall("tei:div", namespace):
+            p = div.findall("tei:p", namespace)
+            result = p
+    return result
+    
+def get_segment_list(file) -> list[Segment]:
+    segments = []
+    namespace = get_namespace()
+    tree = ET.parse(file)
+    root = tree.getroot()
+    for body in root.findall(".//tei:body", namespace):
+        for div in body.findall("tei:div", namespace):
+            segment = Segment()
+            header = div.find("tei:head", namespace)
+            header_number = header.get("n")
+            if(header_number == None):
+                header_number = ""
 
+            print("header: " + header.text + " " + header_number)
+            segment.set_header(header.text)
+
+            content = ""
+            for p in div.findall("tei:p", namespace):
+                content.join([p.text, "\n"])
+            segment.set_content(content)
+        print(segment)
+        segments.append(segment)
+    return segments
+
+def iter_and_print(elem):
+    for e in elem.iter():
+        print(e)
+    
+def get_namespace() -> dict:
+    return {'tei':'http://www.tei-c.org/ns/1.0'}
 
 if __name__ == "__main__":
-    print(SEGMENT_FOLDER)
     parse_xml_folder()
