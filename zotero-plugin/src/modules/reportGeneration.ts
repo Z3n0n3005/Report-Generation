25ff904f64f89af5ee69864319598db5e9ac5d99
Vy <z3n0n3005@gmail.com>
2024-05-04 14:02:26 +0700

    Done zotero-plugin (minimum viable)

--- zotero-plugin/src/modules/reportGeneration.ts
+++ zotero-plugin/src/modules/reportGeneration.ts
@@ -20,11 +20,11 @@ export class ReportGenerationFactory{
     static readonly API_KEY = "IqssCl6uXkPQqcMP6y52Enj2"
     static readonly LIBRARY_ID = "14142718"
     static readonly LIBRARY_TYPE = "user"
-    // static readonly ITEM_KEY = "SNXV9A8F"
-    static readonly ITEM_KEY = "3XNTQMIM"
+    static readonly ITEM_KEY = "SNXV9A8F"
+    // static readonly ITEM_KEY = "3XNTQMIM"
     static readonly IP_LOCAL_HOST = "127.0.0.1"
     static readonly IP_LOCAL_ROUTER = "192.168.100.28"
-    static readonly URL_BASE = "http://" + this.IP_LOCAL_HOST + ":5000"
+    static readonly URL_BASE = "http://" + this.IP_LOCAL_ROUTER + ":5000"
     static readonly URL_SUMMARY = "/summarize"
     static readonly URL_UPLOAD = "/upload"
     static readonly URL_GET_PDF_FILE_ZOTERO = "/getPdfFileZotero"
@@ -34,24 +34,27 @@ export class ReportGenerationFactory{
     private static generateSummaries() { 
         const zp = Zotero.getActiveZoteroPane();
         const sortedItems = zp.getSortedItems()
-        ztoolkit.log("[ReportGen] selectedItemsLength: " + sortedItems.length)
+        // ztoolkit.log("[ReportGen] selectedItemsLength: " + sortedItems.length)
         
         // this.setSummarizeButton()
         const keyList = []
         const itemKeyPair:{[key:string]: Zotero.Item} = {}
         for (let index = 0; index < sortedItems.length; index++) {
-            ztoolkit.log("[ReportGen] index: " + index + " " + sortedItems.length)
             const item = sortedItems[index];
+            // ztoolkit.log("display titles")
+            // ztoolkit.log(item.getDisplayTitle())
             const pdfFilePath = this.getFilePath(item)
-            
+            const pdfFileItemKey = this.getFileItemKey(item)
+
             // if could not get file path
-            if(!pdfFilePath){ continue }
-            keyList.push(item.key)
-            ztoolkit.log("From self")
-            ztoolkit.log(item)
-            itemKeyPair[item.key] = item
-            ztoolkit.log("From dictionary:")
-            ztoolkit.log(itemKeyPair[item.key])
+            if(!pdfFilePath || !pdfFileItemKey){ continue }
+
+            keyList.push(pdfFileItemKey)
+            // ztoolkit.log("From self")
+            // ztoolkit.log(item)
+            itemKeyPair[pdfFileItemKey] = item
+            // ztoolkit.log("From dictionary:")
+            // ztoolkit.log(itemKeyPair[item.key])
             ztoolkit.log("Dictionary: ")
             ztoolkit.log(itemKeyPair)
         }
@@ -60,12 +63,12 @@ export class ReportGenerationFactory{
     }
 
     @reportGen
-    private static getFilePath(item:Zotero.Item):string|boolean{
-        ztoolkit.log("[ReportGen] isRegualarItem: " + item.isRegularItem())
+    private static getFilePath(item:Zotero.Item):string|false{
+        // ztoolkit.log("[ReportGen] isRegualarItem: " + item.isRegularItem())
         // if the item is not a pdf
         if(!item.isRegularItem()){ return false }
 
-        ztoolkit.log("[ReportGen] hasSummaryNote: " + this.hasSummaryNote(item))
+        // ztoolkit.log("[ReportGen] hasSummaryNote: " + this.hasSummaryNote(item))
         // if item already has a summary note
         if(this.hasSummaryNote(item)){ return false }
 
@@ -74,6 +77,24 @@ export class ReportGenerationFactory{
         // if could not get file path
         if(!pdfFilePath){ return false }
         return pdfFilePath
+    }
+
+    @reportGen
+    private static getFileItemKey(item:Zotero.Item):string|false{
+        if(!item.isRegularItem()){ return false }
+
+        if(this.hasSummaryNote(item)){ return false }
+
+        const attachmentIDs = item.getAttachments()
+        for (let attachmentIndex = 0; attachmentIndex < attachmentIDs.length; attachmentIndex++){
+            const attachmentID = attachmentIDs.at(attachmentIndex)
+            if(attachmentID === undefined){ continue }
+
+            const attachment = Zotero.Items.get(attachmentID)
+            if(!attachment.isPDFAttachment()){ continue }
+            return attachment.key
+        }
+        return false
     }
 
     @reportGen
@@ -88,7 +109,7 @@ export class ReportGenerationFactory{
             }
             const note = Zotero.Items.get(noteID);
             const noteTitle = note.getNoteTitle()
-            ztoolkit.log("[ReportGen] " + noteTitle);
+            // ztoolkit.log("[ReportGen] " + noteTitle);
             if(noteTitle === NoteTitle){
                 return true
             }
@@ -113,10 +134,10 @@ export class ReportGenerationFactory{
 
     @reportGen
     private static getPDFFilePath(item:Zotero.Item):string|false{
-        ztoolkit.log("userID: " + Zotero.Users.getCurrentUserID())
-        ztoolkit.log("itemID: " + item.key)
+        // ztoolkit.log("userID: " + Zotero.Users.getCurrentUserID())
+        // ztoolkit.log("itemID: " + item.key)
         const attachmentIDs = item.getAttachments()
-        ztoolkit.log(attachmentIDs)
+        // ztoolkit.log(attachmentIDs)
         for (let attachmentIndex = 0; attachmentIndex < attachmentIDs.length; attachmentIndex++){
             const attachmentID = attachmentIDs.at(attachmentIndex);
             if(attachmentID === undefined){ continue; }
@@ -132,34 +153,42 @@ export class ReportGenerationFactory{
         return false
     }
 
+    
+
     // This method is currently under construction, might not even work at all
     @reportGen
     private static summarize(keyList:string[], itemKeyPair:{[key:string]:Zotero.Item}){
         const postPdfFileList:Array<Promise<any>> = []
-        // const worker = new Worker("./postZoteroFileThread.ts")
 
-//        keyList.forEach((key) => {
-//           postPdfFileList.push(this.postZoteroFile(key))
-//        })
-
-        // Promise.
-        // postPdfFileList.push(this.postZoteroFile(keyList[0]))
-        const result = this.postZoteroFile(this.ITEM_KEY)
-        result.then((res) => {
-            ztoolkit.log(res)
-//            return this.getSummaryResult()
-        }).then((res) =>  {
-//            ztoolkit.log(res[0].segments)
-//            const item = itemKeyPair[this.ITEM_KEY]
-//            ztoolkit.log(item)
-//            if(item !== undefined){
-//                this.addSummaryNote(item, res)
-//            }
+        keyList.forEach((key) => {
+            ztoolkit.log(key)
+            postPdfFileList.push(this.postZoteroFile(key))
         })
-        // postPdfFileList.push(this.postZoteroFile(this.ITEM_KEY))
-        // Promise.all(postPdfFileList).then((res) => {
-        //     this.getSummaryResult()
-        // })
+
+        Promise.allSettled(postPdfFileList)
+        .then((res) => {
+            // ztoolkit.log(res)
+            return this.getSummaryResult()
+        })
+        .then((res) =>  {
+            // ztoolkit.log(res[0])
+            res.forEach((sum:{id:string, name:string, abstract_seg:string, segments:[{header:string, content:string}]}) => {
+                const itemKey = String(sum.id)
+                const item = itemKeyPair[itemKey]
+                const segments = sum.segments
+                ztoolkit.log(sum)
+                ztoolkit.log(itemKey + " " + this.ITEM_KEY)
+                ztoolkit.log(itemKey === this.ITEM_KEY)
+                ztoolkit.log(item)
+                ztoolkit.log(itemKeyPair)
+                // ztoolkit.log(itemKeyPair[this.ITEM_KEY])
+                if(item !== undefined){
+                    this.addSummaryNote(item, segments)
+                } else {
+                    ztoolkit.log("item is undefined " + itemKey)
+                }
+            })
+        })
     }
 
     @reportGen
@@ -172,42 +201,6 @@ export class ReportGenerationFactory{
         standAloneNote.setNote(content)
     }
 
-    @reportGen
-    private static getFileName(path:string){
-        return path.replace(/^.*(\\|\/|\:)/, '')
-    }
-
-    @reportGen
-    private static async sendPdfFilesAsync(file: File):Promise<boolean>{
-        const formData = new window.FormData()
-        ztoolkit.log(formData)
-        
-        if(!file.type.startsWith('application/pdf')){
-            throw new Error("Invalid file type: ${file.name}.")
-        }
-
-        formData.append('files', file)
-
-        try {
-            const response = await fetch(
-                this.URL_BASE + this.URL_UPLOAD, 
-                {
-                    method: 'POST', 
-                    body: formData, 
-                }
-            );
-
-            if(!response.ok){
-                return false
-            }
-            return true
-        }
-        catch(error){
-            throw error
-        }
-
-    }
-    
     @reportGen
     private static async postZoteroFile(itemKey:string):Promise<any>{
         try{
@@ -248,20 +241,26 @@ export class ReportGenerationFactory{
     }
 
     @reportGen
-    private static addSummaryNote(item:Zotero.Item, res:any){
-        const htmlPrefix = "<div data-schema-version=\"8\">";
+    private static addSummaryNote(item:Zotero.Item, segments:[{header:string, content:string}]){
+        const htmlPrefix = "<div data-schema-version=\"9\">";
         const htmlSufix = "</div>"
-        const htmlNoteTitle = "<p>" + NoteTitle + "<\p>";
+        const htmlNoteTitle = "<p>" + NoteTitle + "</p>";
         var htmlContent = ""
-        var contents = res[0].segments
+        var contents = segments
         contents.forEach((content:any) => {
-            htmlContent += "<strong><p>" + content.header + "</p></strong>><br>"
+            htmlContent += "<strong><p>" + content.header + "</p></strong>"
             htmlContent += "<p>" + content.content + "</p>"
         })
 
         const htmlFinal = htmlPrefix + htmlNoteTitle + htmlContent + htmlSufix
         var noteItem = new Zotero.Item("note")
+        const zp = Zotero.getActiveZoteroPane()
+        const sortedItems = zp.getSortedItems()
+        ztoolkit.log(htmlFinal)
         noteItem.setNote(htmlFinal)
+        noteItem.parentID = item.id
+        item.saveTx()
+        noteItem.saveTx()
         // item.(htmlFinal)
     }
 
